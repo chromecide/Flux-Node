ef3d9ddddbeb0a6e5feb9af70613ad7380541958
Justin Pradier <chromecide@chromecide.com>
2013-01-03 01:23:25 +1100

    basic mdns discovery example

---
+++ Examples/Core/MDNS/Node1.js
@@ -0,0 +1,27 @@
+
+var FluxNode = require('../../../FluxNode.js').FluxNode;
+	new FluxNode({
+		name: 'Fake Service Node 1',
+		host: '0.0.0.0',
+		port: 9000,
+		listeners:{
+			'MDNS.Service.Up': function(service){
+				if(service.type.name=='FluxNode'){
+					console.log('FluxNode Service Up(port: '+service.port+'): '+service.name);	
+				}
+			},
+			'MDNS.Service.Down': function(service){
+				if(service.type.name=='FluxNode'){
+					console.log('FluxNode Service Down(port: '+service.port+'): '+service.name);	
+				}
+			}
+		},
+		mixins:[
+			{
+				name: 'fs_mdns',
+				
+			}
+		]
+	}, function(myNode){
+		console.log('Node Ready');
+	});
