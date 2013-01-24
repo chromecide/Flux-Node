35cf8c0e4cfbc82c52df81ddb8b1cb86582e6f3d
Justin Pradier <chromecide@chromecide.com>
2013-01-24 22:34:48 +1100

    basic example of auto discovery and connection on a locsl network

---
+++ Examples/Applets/AutoDiscovery/index.js
@@ -0,0 +1,63 @@
+var FluxNode = require('../../../FluxNode').FluxNode;
+var os = require('os');
+
+new FluxNode({
+	name: os.hostname(),
+	listeners:{
+		'MDNS.Service.Up': function(advert){
+			var thisNode = this;
+			if(advert.type.name=='FluxNode'){
+				console.log('FluxNode Found');
+				//console.log(advert);
+				if(advert.port!=thisNode.getSetting('TCPTunnels.port')){
+					console.log(advert.addresses[0]);
+					console.log(advert.port);
+					var connectionCfg = {
+						host: advert.addresses[0],
+						port: advert.port
+					};
+					thisNode.TCPTunnels_Connect(connectionCfg);	
+				}else{
+					console.log('oh wait...that\'s me...');
+				} 
+				
+			}
+		},
+		'MDNS.Service.Down': function(advert){
+			if(advert.type.name=='FluxNode'){
+				console.log('FluxNode Dissappeared');
+			}
+		}
+	},
+	mixins:[
+		{
+			name: 'debug',
+			options: {
+				events: true
+			}
+		},
+		{
+			name: 'TCPTunnels',
+			options:{
+				host: '0.0.0.0',
+				port: 'auto'
+			}
+		},
+		{
+			name: 'fs_mdns',
+			options:{
+				browsers: [
+					{
+						name: 'FluxNode_Browser',
+						protocol: 'tcp'
+					}
+				]
+			}
+		}
+	]	
+}, function(thisNode){
+	console.log('LISTENING on port '+thisNode.getSetting('TCPTunnels.port'));
+	thisNode.MDNS_createAdvert('FluxNode', 'tcp', 'FluxNode', thisNode.getSetting('TCPTunnels.port'), function(){
+		console.log(arguments);
+	});
+});
