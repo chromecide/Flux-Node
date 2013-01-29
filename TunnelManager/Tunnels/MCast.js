7ae8146b3498155ad2ba2c539808219bb17c7fbf
Justin Pradier <chromecide@chromecide.com>
2013-01-29 21:48:46 +1100

    initialisartion Updates

--- TunnelManager/Tunnels/MCast.js
+++ TunnelManager/Tunnels/MCast.js
@@ -14,22 +14,68 @@ function mcastTunnelBuilder(util, EventEmitter2, Tunnel){
 		
 		Tunnel.call(this, arguments);
 		
+		self.status = 'closed';
 		self.setServer = setServer;
 		self.send = send;
 		self.recieve = recieve;
 		self.close = close;
+		self.init = init;
+		self.initRecieve = initRecieve;
+		self.recievedMessages = {};
 	}
 	
 		util.inherits(MCastTunnel, Tunnel);
+	
+	function init(doInit){
+		//send an init call and wait for an init_response
+		var self = this;
+		
+		self.status = 'pending';
+		if(doInit){
+			self.server.once('message', function(data){
+				self.initRecieve(data);
+			});
+			
+			self.send({
+				topic: 'init'
+			});	
+		}else{
+			self.send({
+				topic: 'init_response'
+			});
+			
+			self.server.on('message', function(data, remote){
+				
+				self.recieve(data);
+			});
+			
+			self.status = 'ready';
+			self.emit('Tunnel.Ready', self);
+		}
+	}
+	
+	function initRecieve(data){
+		var self = this;
+		var message = JSON.parse(data.toString());
+		if(message.topic==self.remoteId+'.'+self.localId+'.init_response'){ //we're waiting for an init_response
+			
+			self.server.on('message', function(data, remote){
+				
+				self.recieve(data);
+			});
+			self.status = 'ready';
+			self.emit('Tunnel.Ready', self);
+		}else{
+			self.server.once('message', function(data){
+				self.initRecieve(data);
+			});
+		}
+	}
 	
 	function setServer(server, cb){
 		var self = this;
 		
 		self.server = server;
-		
-		server.on('message', function(data){
-			self.recieve(data);
-		});
 		
 		return true;
 	}
@@ -37,8 +83,7 @@ function mcastTunnelBuilder(util, EventEmitter2, Tunnel){
 	function send(message){
 		var server = this.server;
 		
-		message.topic = this.localId+'.'+this.peerId+'.'+message.topic;
-		
+		message.topic = this.localId+'.'+this.remoteId+'.'+message.topic;
 		var buff = new Buffer(JSON.stringify(message));
 		server.send(buff);
 	}
@@ -47,22 +92,34 @@ function mcastTunnelBuilder(util, EventEmitter2, Tunnel){
 		var self = this;
 		var message = JSON.parse(data);
 		
-		var topicParts = message.topic.split('.');
-		var peerId = topicParts[0];
-		if(message.topic == peerId+'.'+this.localId+'.MCastTunnels.Announce' || message.topic == peerId+'.*.MCastTunnels.Announce'){
-			//do nothing
-			this.peerId = peerId;
+		if(self.status=='ready'){
+			if(message._message && message._message.id && !self.recievedMessages[message._message.id]){// already recieved this one
+				if(message.sender==self.remoteID){
+					self.recievedMessages[message._message.id] = true;
+					var topicParts = message.topic.split('.');
+					var peerId = topicParts[0];
+					if(peerId!=this.localId){
+						if(message.topic == peerId+'.'+this.localId+'.MCastTunnels.Announce' || message.topic == peerId+'.*.MCastTunnels.Announce'){
+							//do nothing
+							//this.peerId = peerId;
+						}else{
+							if(topicParts[1]=='*' || topicParts[1]==this.localId && topicParts[0]==self.remoteId){ //addressed to anyone, or to this tunnel directly
+								
+								message.topic = message.topic.replace(peerId+'.*.', '').replace(peerId+'.'+this.localId+'.', '');
+								self.emit('message', self, message);	
+							}
+						}	
+					}	
+				}
+			}	
 		}else{
-			if(topicParts[1]=='*' || topicParts[1]==this.localId){ //addressed to anyone, or to this tunnel directly
-				
-				message.topic = message.topic.replace(peerId+'.*.', '').replace(peerId+'.'+this.localId+'.', '');
-				self.emit('message', self, message);	
-			}
+			console.log('TUNNEL NOT READY');
 		}
 	}
 	
 	function close(){
-		
+		var self = this;
+		self.status = 'closed';
 	}
 	return MCastTunnel;
 }
