8c5c0c08a97054f56ec5a8028b42698755b6092b
Justin Pradier <chromecide@chromecide.com>
2013-02-10 14:18:58 +1100

    updated example with information about Windows machines

--- Examples/Core/Tunnels/MCast/index.js
+++ Examples/Core/Tunnels/MCast/index.js
@@ -1,7 +1,7 @@
 var FluxNode = require('../../../../FluxNode').FluxNode;
 
 new FluxNode({
-	id: 'Node1', //change this for each instance you start
+	id: 'Node2', //change this for each instance you start
 	//debug: true,
 	listeners:{
 		'Tunnel.Ready': function(destination, tunnel){
@@ -25,7 +25,12 @@ new FluxNode({
 			}
 		},
 		{
-			name: 'MCastTunnels'
+			name: 'MCastTunnels',
+			options:{
+				server:{
+					address: '0.0.0.0'//this needs to be set to the local ip address when run on Windows based machines
+				}
+			}
 		}
 	]
 }, function(thisNode){
